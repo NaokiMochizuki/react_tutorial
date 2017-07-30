ff168bda8fedb7348c4dac77f7fed6dfdf688b89
NaokiMochizuki <naoki.mochizuki1006@gmail.com>
2017-07-30 12:47:25 +0900

    propsとprops.childrenで値の受け渡し

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -14,7 +14,8 @@ var CommentList = React.createClass({
   render: function(){
     return(
       <div className='commentList'>
-        Hello world! I am a CommentList
+        <Comment author='Pate Hunt'>This is one Commnet</Comment>
+        <Comment author='Jordan Walke'>This is *another* comment</Comment>
       </div>
     );
   }
@@ -25,6 +26,23 @@ var CommentForm = React.createClass({
     return(
       <div className='commnetForm'>
         Hello world! I am a CommentForm
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function(){
+    //markdownからHTMLにコンバートするライブラリ、remarkable.jsの使用
+    var md = new Remarkable();
+    //propsでは属性として渡された値を取得
+    //props.childrenでは、間に挟まれたノード(この場合はThis is ~~~っていう文字)を渡してるっぽい
+    return(
+      <div className='comment'>
+        <h2 className='commentAuthor'>
+          { this.props.author }
+        </h2>
+        { md.render(this.props.children.toString()) }
       </div>
     );
   }
